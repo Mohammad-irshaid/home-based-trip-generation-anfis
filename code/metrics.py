bc7acca631ac4d1a863d0541828f8217ff077a14
Mohammad Irshaid <87867008+Mohammad-irshaid@users.noreply.github.com>
2026-05-14 13:14:57 +0300

    Create metrics.py

---
+++ code/metrics.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_error,
+    r2_score
+)
+
+
+def evaluate_model(y_true, y_pred):
+
+    rmse = np.sqrt(
+        mean_squared_error(y_true, y_pred)
+    )
+
+    mae = mean_absolute_error(
+        y_true,
+        y_pred
+    )
+
+    r2 = r2_score(
+        y_true,
+        y_pred
+    )
+
+    return {
+        "RMSE": round(rmse, 4),
+        "MAE": round(mae, 4),
+        "R2": round(r2, 4)
+    }
